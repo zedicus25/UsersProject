1f332aad6c6f333828c5a8096fd5a7d964ef7d4f
Alexander Tsyganok <Ciga_fu80@student.itstep.org>
2021-12-21 18:46:48 +0200

    Add class user

---
+++ ConsoleApplication1/c_User.cpp
@@ -0,0 +1,25 @@
+#include "c_User.h"
+
+c_User::c_User()
+{
+	this->id = 0;
+	this->name = "none";
+	this->last_name = "none";
+	this->age = 0;
+}
+
+c_User::c_User(long id, std::string name, std::string last_name, int age)
+{
+	this->id = id;
+	this->name = name;
+	this->last_name = name;
+	this->age = age;
+}
+
+std::ostream& operator<<(std::ostream& out, const c_User us)
+{
+	out << "Last name: " << us.getLastName() << "\n";
+	out << "Name: " << us.getName() << "\n";
+	out << "Age: " << us.getAge() << "\n";
+	return out;
+}
